3ef3a878b678155da79ec295e2c5ff568c7579bd
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2024-05-25 13:56:47 +0530

    feeds moved to /home routes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,10 +2,7 @@ import Navbar from "@/components/Navbar/Navbar";
 import HomePage from "@/components/home-page.tsx";
 
 export default function Home() {
-
-  const test = useSelector(( state : any ) => state.counter.value )
-  const dispatch = useDispatch();
-
+  
   return (
     <div>
       <Navbar/>
