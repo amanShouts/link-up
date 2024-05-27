ff8f4289e3a6b451540c757c870ca477051940bb
prathamdupare <prathamdupare@proton.me>
2024-05-27 15:26:42 +0530

    added onboardingroute

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { getAllUsersRoute, saveUserRoute } from './routes/userRoutes';
-const cors = require('cors')
-import mentorRoutes from './routes/mentorRoutes';
+import express from "express";
+import dotenv from "dotenv";
+import {
+  getAllUsersRoute,
+  onboardingRoute,
+  saveUserRoute,
+} from "./routes/userRoutes";
+const cors = require("cors");
+import mentorRoutes from "./routes/mentorRoutes";
 
 dotenv.config();
 
@@ -13,19 +17,20 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json({
-        msg: "Server is fine"
-    })
-})
+  res.json({
+    msg: "Server is fine",
+  });
+});
 
-app.use('/api/mentor', mentorRoutes);
+app.use("/api/mentor", mentorRoutes);
+app.use("/api/onboarding", onboardingRoute);
 
-// /users 
-app.use(getAllUsersRoute)
+// /users
+app.use(getAllUsersRoute);
 
 // save-user
-app.use(saveUserRoute)
+app.use(saveUserRoute);
 
 app.listen(PORT, () => {
-    console.log("Server running on port ", PORT)
-})
+  console.log("Server running on port ", PORT);
+});
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { getUsers, saveUserController } from "../controllers/userController";
+import {
+  getUsers,
+  saveUserController,
+  userOnboarding,
+} from "../controllers/userController";
 
 export const getAllUsersRoute = Router();
 export const saveUserRoute = Router();
+export const onboardingRoute = Router();
 
-getAllUsersRoute.get('/users',getUsers);
-saveUserRoute.post('/save-user',saveUserController)
+getAllUsersRoute.get("/users", getUsers);
+saveUserRoute.post("/save-user", saveUserController);
+onboardingRoute.put("/", userOnboarding);
