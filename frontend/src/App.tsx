41cb2ed10a5ba253d6ba80eee95558b9171aa3f5
Abhimanyu Yadav <abhi@Abhimanyus-MacBook-Air.local>
2024-05-30 14:05:22 +0530

    connecting small-profile to backend and adding skeleton as well

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,11 +11,11 @@ import Profile from "./pages/MentorProfile";
 import MentorList from "./pages/MentorList";
 import MentorProfile from "./pages/MentorProfile";
 import Navbar from "./components/Navbar/Navbar";
-import { closeModal, modalDetails, openModal } from './store/slice/modalSlice';
+import { modalDetails, openModal } from "./store/slice/modalSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { addUserDetails } from "./store/slice/userSlice";
 import { BACKEND_URL } from "./config";
-import { RootState } from './store/store'
+import { RootState } from "./store/store";
 import axios from "axios";
 import EditProfile from "./pages/EditProfile";
 import DetailsModal from "./components/modalStore/DetailsModal";
@@ -24,47 +24,51 @@ export default function App() {
   const { isSignedIn, isLoaded, user } = useUser();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const storedUsers = useSelector(( state: RootState ) => state.users.users );
+  const storedUsers = useSelector((state: RootState) => state.users.users);
 
-  const toggleModal = ( type: String ) => {
-    dispatch(modalDetails(type))
-    dispatch(openModal())
-  }
+  const toggleModal = (type: String) => {
+    dispatch(modalDetails(type));
+    dispatch(openModal());
+  };
 
   useEffect(() => {
     const currentRoute = window.location.pathname;
 
-    const publicRoutes = ['/','/signup'];
-    
+    const publicRoutes = ["/", "/signup"];
+
     if (isLoaded && !isSignedIn && !publicRoutes.includes(currentRoute)) {
       navigate("/login");
     }
-    if ( isSignedIn && user ) {
-      console.log("user: ",user)
+    if (isSignedIn && user) {
+      console.log("user: ", user);
 
-      const currentUser = storedUsers.find((el) => el.username === user.username);
+      const currentUser = storedUsers.find(
+        (el) => el.username === user.username,
+      );
 
-      if ( currentUser && ( currentUser?.age === null || currentUser?.city === null || currentUser?.userType == null ) ) {
-        console.log("here")
-        toggleModal("edit-profile-modal")
+      if (
+        currentUser &&
+        (currentUser?.age === null ||
+          currentUser?.city === null ||
+          currentUser?.userType == null)
+      ) {
+        console.log("here");
+        toggleModal("edit-profile-modal");
       }
-
+    } else {
+      console.log("user not signied in");
     }
-    else {
-      console.log("user not signied in")
-    }
-
-  }, [isLoaded, isSignedIn, navigate, window.location ]);
+  }, [isLoaded, isSignedIn, navigate, window.location]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(BACKEND_URL + '/users');
+        const response = await fetch(BACKEND_URL + "/users");
         const responseData = await response.json();
 
         dispatch(addUserDetails(responseData));
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -74,27 +78,24 @@ export default function App() {
   useEffect(() => {
     const usernameArray = storedUsers.map((el) => {
       return el.username;
-    })
-    
-    if ( user && user.username && !usernameArray.includes(user.username) ) {
-     
-      axios.post(BACKEND_URL+'/save-user',{
-        "username": user?.username,
-        "name": user.firstName,
-        "img":user.imageUrl,
-        "lastLogin": user.lastSignInAt,
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    });
 
+    if (user && user.username && !usernameArray.includes(user.username)) {
+      axios
+        .post(BACKEND_URL + "/save-user", {
+          username: user?.username,
+          name: user.firstName,
+          img: user.imageUrl,
+          lastLogin: user.lastSignInAt,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-
-  }, [storedUsers, user])
-  
+  }, [storedUsers, user]);
 
   if (!isLoaded && window.location.pathname !== "/home") {
     return (
@@ -112,19 +113,28 @@ export default function App() {
 
   return (
     <main className="dark:bg-black dark:text-white">
-      <Navbar/>
-      <DetailsModal/>
+      <Navbar />
+      <DetailsModal />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<CustomSignIn />} />
         <Route path="/signup" element={<CustomSignUp />} />
         <Route path="/signup/continue" element={<CustomSignIn />} />
-        <Route path="/onboarding" element={isSignedIn ? <Onboarding /> : null} />
-        <Route path="/home" element={isSignedIn ? <Home/> : null} />
+        <Route
+          path="/onboarding"
+          element={isSignedIn ? <Onboarding /> : null}
+        />
+        <Route path="/home" element={isSignedIn ? <Home /> : null} />
         <Route path="/profile" element={isSignedIn ? <Profile /> : null} />
-        <Route path="/edit-profile" element={isSignedIn ? <EditProfile /> : null} />
-        <Route path="/mentors" element={isSignedIn ? <MentorList/> : null} />
-        <Route path="/mentor/:mentorId" element={isSignedIn ? <MentorProfile/> : null} />
+        <Route
+          path="/edit-profile"
+          element={isSignedIn ? <EditProfile /> : null}
+        />
+        <Route path="/mentors" element={isSignedIn ? <MentorList /> : null} />
+        <Route
+          path="/mentor/:mentorId"
+          element={isSignedIn ? <MentorProfile /> : null}
+        />
       </Routes>
     </main>
   );
---
+++ frontend/src/lib/fetchers/getSingleUserDetail.ts
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+export const getSingleUserDetail = (url:string) => axios.get(url).then(res => res.data)
