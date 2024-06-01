6f6ad33db975dcf541b2e1d9fc6798f26ed96f8c
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2024-06-01 20:31:29 +0530

    onboarding updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import {
   getSingleUser,
   onboardingRoute,
   saveUserRoute,
+  updateUserIndustriesRoute,
+  updateUserSkillsRoute,
 } from "./routes/userRoutes";
 const cors = require("cors");
 import mentorRoutes from "./routes/mentorRoutes";
@@ -43,6 +45,12 @@ app.use(saveUserRoute);
 
 //edit-user
 app.use(onboardingRoute);
+
+// skills
+app.use(updateUserSkillsRoute);
+
+// industries
+app.use(updateUserIndustriesRoute);
 
 app.listen(PORT, () => {
   console.log("Server running on port ", PORT);
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,6 +4,8 @@ import {
   getUserProfile,
   getUsers,
   saveUserController,
+  updateUserIndustriesController,
+  updateUserSkillsController,
   userOnboarding,
 } from "../controllers/userController";
 
@@ -12,8 +14,12 @@ export const saveUserRoute = Router();
 export const onboardingRoute = Router();
 export const getSingleUser = Router();
 export const getUserDetailsByUsername = Router();
+export const updateUserSkillsRoute = Router();
+export const updateUserIndustriesRoute = Router();
 
 getAllUsersRoute.get("/users", getUsers);
 saveUserRoute.post("/save-user", saveUserController);
 onboardingRoute.put("/edit-user", userOnboarding);
+updateUserSkillsRoute.post("/skills", updateUserSkillsController);
+updateUserIndustriesRoute.put("/industries", updateUserIndustriesController);
 getSingleUser.get("/:id", getSingleUserController);
